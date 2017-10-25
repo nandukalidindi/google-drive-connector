edeed9441c9a5992eae80fe6259c747a3cf12655
nandukalidindi <nandukalidindi@gmail.com>
2017-10-25 11:37:20 -0400

    Move authentication mechanism into its own class

--- auth/oauth2.py
+++ auth/oauth2.py
@@ -1,9 +1,50 @@
+from requests_oauthlib import OAuth2Session
 
+OAUTH2_AUTH_URL = "https://accounts.google.com/o/oauth2/auth"
+OAUTH2_TOKEN_URL = "https://accounts.google.com/o/oauth2/token"
+
+SCOPE_DICT = { "DRIVE READONLY": "https://www.googleapis.com/auth/drive.readonly" }
 
 class OAuth2:
-	def __init__(self):
-		pass
+	def __init__(self, client_id, client_secret, redirect_uri, scope):
+		self.client_id = client_id
+		self.client_secret = client_secret
+		self.redirect_uri = redirect_uri
+		self.scope = SCOPE_DICT.get(scope)
+		self.initialize_oauth2_session()
+
+	def initialize_oauth2_session(self):
+		try:
+			self.oauth2_session = OAuth2Session(self.client_id, redirect_uri=self.redirect_uri, scope=self.scope)
+		except:
+			self.oauth2_session = None
 
 
-	def clientsecrets_flow():
-		 pass
+	def get_authorization_url(self):
+		try:
+			auth_url, state = self.oauth2_session.authorization_url(OAUTH2_AUTH_URL)
+		except:
+			auth_url = None
+
+		return auth_url
+
+	def fetch_access_token(self, auth_code):
+		try:
+			token_obj = self.oauth2_session.fetch_token(token_url=OAUTH2_TOKEN_URL, code=auth_code, client_secret=self.client_secret)
+			access_token = token_obj.get('access_token')
+		except:
+			access_token = None
+
+		return access_token
+
+	@staticmethod
+	def get_active_session(self):
+		if self.oauth2_session:
+			return self.oauth2_session
+		else:
+			self.initialize_oauth2_session()
+			return self.oauth2_session
+
+
+
+
--- connector.py
+++ connector.py
@@ -1,30 +1,19 @@
-REDIRECT_URI = "http://localhost:5000/google_oauth2/callback"
+from credentials import client_id, client_secret, redirect_uri
+from flask import Flask, render_template, request, redirect, session
+from auth.oauth2 import OAuth2
 
-access_token = ""
-
-import pdb
-from credentials import client_id, client_secret
-from flask import Flask, render_template, request, redirect
-from requests_oauthlib import OAuth2Session
-
-
-scope = ['https://www.googleapis.com/auth/drive.readonly']
-oauth = OAuth2Session(client_id, redirect_uri=REDIRECT_URI, scope=scope)
+oauth2_session = OAuth2(client_id, client_secret, redirect_uri, "DRIVE READONLY")
 
 app = Flask(__name__)
+app.secret_key = 'super secret key'
 
 @app.route('/')
 def connect():
-    return render_template('connector.html', auth_url=authorization_url())
+	return render_template('connector.html', auth_url=oauth2_session.get_authorization_url())    
 
 @app.route('/google_oauth2/callback')
 def oauth2_callback():
-	authorization_code = request.values['code']
-	token = oauth.fetch_token(token_url="https://accounts.google.com/o/oauth2/token", code=authorization_code, client_secret=client_secret)
-	access_token = token.get('access_token')	
+	auth_code = request.values['code']
+
+	access_token = oauth2_session.fetch_access_token(auth_code)
 	return redirect("http://localhost:5000/", code=302)
-
-
-def authorization_url():	
-	authorization_url, state = oauth.authorization_url('https://accounts.google.com/o/oauth2/auth')
-	return authorization_url
